70c509f3b651f47abb2a5189013af51e38beda3e
Jamie Maguire <jamieolivermaguire@googlemail.com>
2023-08-11 21:03:16 +0100

    pwa

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Define a name for the current cache version
+const cacheName = 'v1';
+
+// List of files to be cached
+const cacheAssets = [
+  './',
+  'index.html'
+];
+
+// Call Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      cache.addAll(cacheAssets);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Call Activate event
+self.addEventListener('activate', (event) => {
+  // Remove unwanted caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
